b7fe515f214c8a8ad293270b36a10b39c6657f58
Thejasshree-K <128981508+Thejasshree-K@users.noreply.github.com>
2023-05-07 11:35:20 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/*
+Weather APP
+
+Name 
+College name 
+
+Email, Location
+1. Today 
+2. Yesterday
+3. Tomorrow
+4. Week report 
+
+
+cloudy
+dry
+rain 
+thunderstrom
+heatwaves
+temperature 
+
+
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
